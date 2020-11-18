30ba2be0316dc168785289afa907798854fbe743
karinpolgari <karinpolgari07@yahoo.com>
2020-11-18 13:02:34 -0800

    Tema 1

---
+++ tema1.js
@@ -0,0 +1,18 @@
+var dog = {
+    name : "Lady",
+    age : 10,
+    color : "white",
+    hasClothes: true
+    
+}
+
+
+function intr (nume)
+
+{   
+    var nume="Karin";
+    return "Buna, numele meu este " +  nume;
+   
+    
+}
+  console.log(intr());
