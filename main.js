44befdaefedeb4b7ed419b5f739f94b42e3718b3
karinpolgari <karinpolgari07@yahoo.com>
2020-11-17 18:59:51 -0800

    curs 1

---
+++ main.js
@@ -0,0 +1,18 @@
+var name = "Karin";
+var ammount = ">7";
+var knowJavaScript = true;
+var animal = ["dog", "cat"]; 
+
+function add() {
+    return 1;
+}
+var person = {
+    heigh:2,
+    canWalk: function () {
+
+    
+        console.log("yea");
+    },
+    name: "kari"
+    
+}
